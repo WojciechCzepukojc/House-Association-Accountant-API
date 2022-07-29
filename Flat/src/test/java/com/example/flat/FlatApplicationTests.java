bcebd75a154cbe5caf56434a546930d04a6f53c2
Wojciech_Czepukojc <wojciechczepukojc@gmail.com>
2022-07-29 21:46:31 +0200

    init project

---
+++ Flat/src/test/java/com/example/flat/FlatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
