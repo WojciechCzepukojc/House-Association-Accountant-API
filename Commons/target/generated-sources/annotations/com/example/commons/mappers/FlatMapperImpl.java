0fb514750601c9c3ac714794e160ae8b1e09bb97
Wojciech_Czepukojc <wojciechczepukojc@gmail.com>
2022-08-04 12:50:53 +0200

    fixed commons

---
+++ Commons/target/generated-sources/annotations/com/example/commons/mappers/FlatMapperImpl.java
@@ -0,0 +1,50 @@
+package com.example.commons.mappers;
+
+import com.example.commons.dto.FlatDto;
+import com.example.commons.model.Flat;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-08-04T12:47:11+0200",
+    comments = "version: 1.5.2.Final, compiler: javac, environment: Java 11.0.15 (Amazon.com Inc.)"
+)
+@Component
+public class FlatMapperImpl implements FlatMapper {
+
+    @Override
+    public FlatDto map(Flat flat) {
+        if ( flat == null ) {
+            return null;
+        }
+
+        FlatDto.FlatDtoBuilder flatDto = FlatDto.builder();
+
+        flatDto.id( flat.getId() );
+        flatDto.streetName( flat.getStreetName() );
+        flatDto.area( flat.getArea() );
+        flatDto.residentsNumber( flat.getResidentsNumber() );
+        flatDto.rent( flat.getRent() );
+
+        return flatDto.build();
+    }
+
+    @Override
+    public Flat map(FlatDto flatDto) {
+        if ( flatDto == null ) {
+            return null;
+        }
+
+        Flat flat = new Flat();
+
+        flat.setId( flatDto.getId() );
+        flat.setStreetName( flatDto.getStreetName() );
+        flat.setFlatNumber( flatDto.getFlatNumber() );
+        flat.setArea( flatDto.getArea() );
+        flat.setResidentsNumber( flatDto.getResidentsNumber() );
+        flat.setRent( flatDto.getRent() );
+
+        return flat;
+    }
+}
