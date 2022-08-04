bdb4514b3b051b53e7c9069579ed45fdeb41f41c
Wojciech_Czepukojc <wojciechczepukojc@gmail.com>
2022-08-04 12:50:46 +0200

    fixed commons

--- Flat/src/main/java/com/example/flat/controllers/FlatController.java
+++ Flat/src/main/java/com/example/flat/controllers/FlatController.java
@@ -1,23 +1,23 @@
-//package com.example.flat.controllers;
-//
-//import com.example.flat.services.FlatService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.web.bind.annotation.*;
-//import com.example.commons.dto.FlatDto;
-//
-//@RestController
-//@RequiredArgsConstructor
-//@RequestMapping("/api/v1/flat")
-//public class FlatController {
-//
-//    private final FlatService flatService;
-//
-//    @PostMapping("create")
-//    @ResponseStatus(HttpStatus.CREATED)
-//    public void create (@RequestBody FlatDto flatDto){
-//        flatService.create(flatDto);
-//    }
-//
-//
-//}
+package com.example.flat.controllers;
+
+import com.example.flat.services.FlatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import com.example.commons.dto.FlatDto;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/flat")
+public class FlatController {
+
+    private final FlatService flatService;
+
+    @PostMapping("create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create (@RequestBody FlatDto flatDto){
+        flatService.create(flatDto);
+    }
+
+
+}
