116cae21c8ac929d685cb70f556367557b076e00
Wojciech_Czepukojc <wojciechczepukojc@gmail.com>
2022-08-27 20:29:53 +0200

    fixed RentAplication

--- Rent/src/main/java/com/example/rent/RentApplication.java
+++ Rent/src/main/java/com/example/rent/RentApplication.java
@@ -2,8 +2,12 @@ package com.example.rent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@EnableEurekaClient
+@EntityScan(basePackages = "com.example.*")
+@SpringBootApplication(scanBasePackages = "com.example.*")
 public class RentApplication {
 
 	public static void main(String[] args) {
