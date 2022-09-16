e1bb13fc863d27dd01297f7dbc72bfa933d7590c
Wojciech_Czepukojc <wojciechczepukojc@gmail.com>
2022-09-16 23:05:55 +0200

    add flats initialization data

---
+++ Flat/src/main/resources/data.sql
@@ -0,0 +1,17 @@
+INSERT INTO flats (id, street_name, flat_number, area, residents_number)
+VALUES
+( 1, 'KATEDRALNA', '1B', 58, 3),
+( 2, 'KATEDRALNA', '1C', 41, 2),
+( 3, 'KATEDRALNA', '4A', 85, 4),
+( 4, 'KATEDRALNA', '8H', 45, 3),
+( 5, 'RATUSZOWA', '7K', 81, 3),
+( 6, 'RATUSZOWA', '12A', 36, 1),
+( 7, 'RATUSZOWA', '2E', 59, 4),
+( 8, 'RATUSZOWA', '1A', 105, 5),
+( 9, 'ZAMKOWA', '9C', 54, 2),
+( 10, 'ZAMKOWA', '8I', 65, 4),
+( 11, 'ZAMKOWA', '4B', 120, 6),
+( 12, 'ZAMKOWA', '2H', 115, 1);
+
+
+    commit;
